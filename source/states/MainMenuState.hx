63740220faf89a169d6a767816c73f96be8810e8
Official3Gamer <140297634+Official3Gamer@users.noreply.github.com>
2026-06-22 07:28:19 -0500

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -14,7 +14,7 @@ enum MainMenuColumn {
 
 class MainMenuState extends MusicBeatState
 {
-	public static var brainyEngineVersion:String = '0.1.0';
+	public static var brainyEngineVersion:String = '0.1.1';
 	public static var psychEngineVersion:String = '1.0.4'; // This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 	public static var curColumn:MainMenuColumn = CENTER;
@@ -94,6 +94,11 @@ class MainMenuState extends MusicBeatState
 			rightItem = createMenuItem(rightOption, FlxG.width - 60, 490);
 			rightItem.x -= rightItem.width;
 		}
+
+		var brainVer:FlxText = new FlxText(12, FlxG.height - 84, 0, "BrainyEnginePlus v" + brainyEngineVersion, 12);
+		brainVer.scrollFactor.set();
+		brainVer.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(brainVer);
 
 		var psychVer:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
 		psychVer.scrollFactor.set();
