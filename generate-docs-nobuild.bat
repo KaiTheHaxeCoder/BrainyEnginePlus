aadd4897209f465e1f41c98b718b188046a62fe9
Brainy7980 <bpurdy12@icloud.com>
2026-06-23 11:40:19 -0700

    alright mb

--- generate-docs-nobuild.bat
+++ generate-docs-nobuild.bat
@@ -1,1 +1,1 @@
-haxelib run dox -i types.xml --title "Brainyverse API Documentation" --include-private -o ./docs
+haxelib run dox -i types.xml -o ./docs/ --title "Brainy Engine+ API documentation" -D themeColor 0x1294f6
